532882c069faf3bf29e2df9d5740edd95d8f6787
github username <github email address>
2018-11-24 03:39:29 -0500

    fourth commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,17 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  //
+  // userIds.forEach(id => dispatch(fetchUser(id)));
 
-  userIds.forEach(id => dispatch(fetchUser(id)));
+  _.chain(getState().posts)
+    .map('userId')
+      .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+          .value();
 };
+
 
 export const fetchPosts = () => {
   return async (dispatch) => {
